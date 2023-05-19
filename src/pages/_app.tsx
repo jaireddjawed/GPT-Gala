0fb3bed6caa5d3152a96d72e1eda9d6aac6b823c
Jaired Jawed <jairedjawed@icloud.com>
2023-05-19 11:41:55 -0700

    added layout code.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-gray-300 dark:bg-gray-800 inset-0 absolute h-full w-full overflow-auto">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+          <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
